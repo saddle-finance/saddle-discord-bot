a3aff4e1f467ab0fda3a3ca0cd6d8c8ede9c3586
Jongseung Lim <penandlim@gmail.com>
2021-04-23 16:11:32 -1000

    Add listeners for adding and removing liquidity

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ async function send(message) {
 }
 
 function toHumanString(rawTokenAmount, decimals, digitsToShow) {
-    let s = BigNumber.from(rawTokenAmount).div(decimals - digitsToShow).toString();
+    let s = BigNumber.from(rawTokenAmount).div(BigNumber.from(10).pow(decimals - digitsToShow)).toString();
     return s.slice(0, -digitsToShow) + "." + s.slice(-digitsToShow);
 }
 
 async function main() {
     bot.on('ready', async () => {
         console.log(`Logged in as ${bot.user.tag}!`);
-        await log(`Bot started at ${await publicIp.v4()}. Using ${process.env.ALCHEMY_API} as the json rpc endpoint.`)
+        log(`Bot started at ${await publicIp.v4()}. Using ${process.env.ALCHEMY_API} as the json rpc endpoint...`)
     });
 
     bot.on('message', msg => {
@@ -41,27 +41,133 @@ async function main() {
     });
 
     for (const contract of contracts) {
-        const instance = new Contract(contract.address, swapABI, provider)
+        const instance = new Contract(contract["address"], swapABI, provider)
 
+        // On token swap event
         instance.on("TokenSwap", (buyer, tokensSold, tokensBought, soldId, boughtId, event) => {
-            const soldTokenName = contract.tokens[soldId]
-            const boughtTokenName = contract.tokens[boughtId]
+            const soldTokenName = contract["tokens"][soldId]
+            const boughtTokenName = contract["tokens"][boughtId]
 
             const digitsToShow = 4
 
-            const numOfTokenSold = toHumanString(tokensSold, contract.decimals[soldId], digitsToShow)
-            const numOfTokenBought = toHumanString(tokensBought, contract.decimals[boughtId], digitsToShow)
+            const numOfTokenSold = toHumanString(tokensSold, contract["decimals"][soldId], digitsToShow)
+            const numOfTokenBought = toHumanString(tokensBought, contract["decimals"][boughtId], digitsToShow)
 
             // inside a command, event listener, etc.
             const embed = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Token swap')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(buyer, null, `https://etherscan.io/address/${buyer}`)
-                .setDescription(`Swapped ${soldTokenName} to ${boughtTokenName}`)
+                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contract["address"]}`)
+                .setDescription(`${buyer} swapped ${soldTokenName} to ${boughtTokenName}`)
                 .addFields(
                     { name: 'Input amount', value: `${numOfTokenSold} ${soldTokenName} ($0)`, inline: true },
                     { name: 'Output amount', value: `${numOfTokenBought} ${boughtTokenName} ($0)`, inline: true },
+                )
+                .addField(`Fees gained by LPs`, "0", false)
+                .setTimestamp()
+                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            send(embed);
+            log(JSON.stringify(event));
+        });
+
+        // On AddLiquidity event
+        instance.on("AddLiquidity", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
+            let depositAmounts = "";
+
+            const digitsToShow = 3
+            for (let i = 0; i < tokenAmounts.length; i++) {
+                depositAmounts += `${contract["tokens"][i]} ${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)}, `
+            }
+            depositAmounts = depositAmounts.slice(0, -2)
+
+            // inside a command, event listener, etc.
+            const embed = new MessageEmbed()
+                .setColor('#33ff33')
+                .setTitle('Deposit')
+                .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
+                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contract["address"]}`)
+                .setDescription(`${provider} added new liquidity to the ${contract['name']}`)
+                .addFields(
+                    { name: 'Deposit amounts', value: `${depositAmounts} ($0)`, inline: false },
+                )
+                .setTimestamp()
+                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            send(embed);
+            log(JSON.stringify(event));
+        });
+
+        // On RemoveLiquidity event
+        instance.on("RemoveLiquidity", (provider, tokenAmounts, lpTokenSupply, event) => {
+            let withdrawAmounts = "";
+
+            const digitsToShow = 3
+            for (let i = 0; i < tokenAmounts.length; i++) {
+                withdrawAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]} , `
+            }
+            withdrawAmounts = withdrawAmounts.slice(0, -2)
+
+            // inside a command, event listener, etc.
+            const embed = new MessageEmbed()
+                .setColor('#FF9A00')
+                .setTitle('Withdraw')
+                .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
+                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contract["address"]}`)
+                .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
+                .addFields(
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
+                )
+                .setTimestamp()
+                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            send(embed);
+            log(JSON.stringify(event));
+        });
+
+        // On RemoveLiquidity event
+        instance.on("RemoveLiquidityOne", (provider, lpTokenAmount, lpTokenSupply, boughtId, tokensBought, event) => {
+            const digitsToShow = 3
+
+            const withdrawAmounts = `${toHumanString(tokensBought, contract["decimals"][boughtId], digitsToShow)} ${contract["tokens"][boughtId]}`;
+
+            // inside a command, event listener, etc.
+            const embed = new MessageEmbed()
+                .setColor('#FF9A00')
+                .setTitle('Withdraw')
+                .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
+                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contract["address"]}`)
+                .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
+                .addFields(
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
+                )
+                .setTimestamp()
+                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            send(embed);
+            log(JSON.stringify(event));
+        });
+
+        // On RemoveLiquidity event
+        instance.on("RemoveLiquidityImbalance", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
+            let withdrawAmounts = "";
+
+            const digitsToShow = 4
+            for (let i = 0; i < tokenAmounts.length; i++) {
+                withdrawAmounts += `${contract["tokens"][i]} ${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)}, `
+            }
+            withdrawAmounts = withdrawAmounts.slice(0, -2)
+
+            // inside a command, event listener, etc.
+            const embed = new MessageEmbed()
+                .setColor('#FF9A00')
+                .setTitle('Withdraw')
+                .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
+                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contract["address"]}`)
+                .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
+                .addFields(
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
