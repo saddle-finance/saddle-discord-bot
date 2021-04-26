5f3bce9c2745c838ca52dd8f4af8bf65860cf774
Jongseung Lim <penandlim@gmail.com>
2021-04-26 12:55:25 -1000

    Add thumbnails to discord messages

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const contracts = require('./constants/contracts.json');
 dotenv.config();
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API);
+const isProduction = !process.env.ALCHEMY_API.includes("127.0.0.1")
 
-const isProduction = process.env.ALCHEMY_API.includes("localhost")
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
 
 function getChannel(channelID) {
     return bot.channels.cache.get(channelID);
@@ -30,6 +34,8 @@ function toHumanString(rawTokenAmount, decimals, digitsToShow) {
     return s.slice(0, -digitsToShow) + "." + s.slice(-digitsToShow);
 }
 
+
+
 async function main() {
     bot.on('ready', async () => {
         console.log(`Logged in as ${bot.user.tag}!`);
@@ -43,13 +49,13 @@ async function main() {
     });
 
     for (const contract of contracts) {
-        const instance = new Contract(contract["address"], swapABI, provider)
+        const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
+        const instance = new Contract(contractAddress, swapABI, provider)
 
         // On token swap event
         instance.on("TokenSwap", (buyer, tokensSold, tokensBought, soldId, boughtId, event) => {
             const soldTokenName = contract["tokens"][soldId]
             const boughtTokenName = contract["tokens"][boughtId]
-            const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
 
             const digitsToShow = 4
 
@@ -61,7 +67,7 @@ async function main() {
                 .setColor('#0099ff')
                 .setTitle('Token swap')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contractAddress}`)
+                .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${buyer} swapped ${soldTokenName} to ${boughtTokenName}`)
                 .addFields(
                     { name: 'Input amount', value: `${numOfTokenSold} ${soldTokenName} ($0)`, inline: true },
@@ -78,23 +84,25 @@ async function main() {
         // On AddLiquidity event
         instance.on("AddLiquidity", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
             let depositAmounts = "";
-            const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
 
             const digitsToShow = 3
             for (let i = 0; i < tokenAmounts.length; i++) {
-                depositAmounts += `${contract["tokens"][i]} ${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)}, `
+                depositAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]} , `
             }
             depositAmounts = depositAmounts.slice(0, -2)
+
+            const totalDollarValue = 0;
 
             // inside a command, event listener, etc.
             const embed = new MessageEmbed()
                 .setColor('#33ff33')
                 .setTitle('Deposit')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contractAddress}`)
+                .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${provider} added new liquidity to the ${contract['name']}`)
                 .addFields(
-                    { name: 'Deposit amounts', value: `${depositAmounts} ($0)`, inline: false },
+                    { name: 'Deposit amounts', value: `${depositAmounts}`, inline: false },
+                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
@@ -106,23 +114,25 @@ async function main() {
         // On RemoveLiquidity event
         instance.on("RemoveLiquidity", (provider, tokenAmounts, lpTokenSupply, event) => {
             let withdrawAmounts = "";
-            const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
 
             const digitsToShow = 3
             for (let i = 0; i < tokenAmounts.length; i++) {
-                withdrawAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]} , `
+                withdrawAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]}, `
             }
             withdrawAmounts = withdrawAmounts.slice(0, -2)
+            const totalDollarValue = 0;
 
             // inside a command, event listener, etc.
             const embed = new MessageEmbed()
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contractAddress}`)
+                .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
-                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
+                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
@@ -134,19 +144,21 @@ async function main() {
         // On RemoveLiquidity event
         instance.on("RemoveLiquidityOne", (provider, lpTokenAmount, lpTokenSupply, boughtId, tokensBought, event) => {
             const digitsToShow = 3
-            const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
-
             const withdrawAmounts = `${toHumanString(tokensBought, contract["decimals"][boughtId], digitsToShow)} ${contract["tokens"][boughtId]}`;
+
+            const totalDollarValue = 0
 
             // inside a command, event listener, etc.
             const embed = new MessageEmbed()
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(contract["name"], null, `https://etherscan.io/address/${contractAddress}`)
+                .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
-                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
+                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
@@ -158,7 +170,6 @@ async function main() {
         // On RemoveLiquidity event
         instance.on("RemoveLiquidityImbalance", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
             let withdrawAmounts = "";
-            const contractAddress = isProduction ? contract["address"] : contract["localAddress"]
 
             const digitsToShow = 4
             for (let i = 0; i < tokenAmounts.length; i++) {
@@ -171,10 +182,12 @@ async function main() {
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
-                .setAuthor(contract["name"], "", `https://etherscan.io/address/${contractAddress}`)
+                .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
-                    { name: 'Withdraw amounts', value: `${withdrawAmounts} ($0)`, inline: false },
+                    { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
+                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
