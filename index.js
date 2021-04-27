e3305df1e83941b3505579de53cbd2610a6bd6b0
Jongseung Lim <penandlim@gmail.com>
2021-04-26 16:30:40 -1000

    Add coingecko prices for all events

--- index.js
+++ index.js
@@ -5,17 +5,37 @@ const { ethers, Contract, BigNumber } = require("ethers");
 const publicIp = require('public-ip');
 const swapABI = require('./constants/swapABI.json');
 const contracts = require('./constants/contracts.json');
+const retry = require('async-retry');
+const fetch = require('node-fetch');
 
 // Set up dotenv config and discord bot
 dotenv.config();
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API);
 const isProduction = !process.env.ALCHEMY_API.includes("127.0.0.1")
+const coinGeckoAPI = "https://api.coingecko.com/api/v3/simple/price"
 
 const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 });
+
+async function queryTokenPricesUSD(tokenIDs) {
+    return await retry(async bail => {
+        // if anything throws, we retry
+        const res = await fetch(`${coinGeckoAPI}?ids=${encodeURIComponent(tokenIDs.join(","))}&vs_currencies=usd`)
+
+        if (403 === res.status) {
+            // don't retry upon 403
+            bail(new Error('Unauthorized'))
+            return
+        }
+
+        return res.json()
+    }, {
+        retries: 5
+    })
+}
 
 function getChannel(channelID) {
     return bot.channels.cache.get(channelID);
@@ -34,7 +54,9 @@ function toHumanString(rawTokenAmount, decimals, digitsToShow) {
     return s.slice(0, -digitsToShow) + "." + s.slice(-digitsToShow);
 }
 
-
+function toUSD(s) {
+    return formatter.format(parseFloat(s))
+}
 
 async function main() {
     bot.on('ready', async () => {
@@ -53,48 +75,66 @@ async function main() {
         const instance = new Contract(contractAddress, swapABI, provider)
 
         // On token swap event
-        instance.on("TokenSwap", (buyer, tokensSold, tokensBought, soldId, boughtId, event) => {
+        instance.on("TokenSwap", async (buyer, tokensSold, tokensBought, soldId, boughtId, event) => {
             const soldTokenName = contract["tokens"][soldId]
             const boughtTokenName = contract["tokens"][boughtId]
 
             const digitsToShow = 4
 
+            const prices = await queryTokenPricesUSD(contract["coingeckoIDs"])
+
             const numOfTokenSold = toHumanString(tokensSold, contract["decimals"][soldId], digitsToShow)
             const numOfTokenBought = toHumanString(tokensBought, contract["decimals"][boughtId], digitsToShow)
 
+            const totalUSDValueSold = toUSD(toHumanString(
+                BigNumber.from(tokensSold).mul(prices[`${contract["coingeckoIDs"][soldId]}`]["usd"]),
+                contract["decimals"][soldId],
+                digitsToShow
+            ))
+            const totalUSDValueBought = toUSD(toHumanString(
+                BigNumber.from(tokensBought).mul(prices[`${contract["coingeckoIDs"][boughtId]}`]["usd"]),
+                contract["decimals"][boughtId],
+                digitsToShow
+            ))
+
             // inside a command, event listener, etc.
-            const embed = new MessageEmbed()
+            let embed = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Token swap')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
                 .setAuthor(contract["name"], contract["authorThumbnailURL"], `https://etherscan.io/address/${contractAddress}`)
                 .setDescription(`${buyer} swapped ${soldTokenName} to ${boughtTokenName}`)
                 .addFields(
-                    { name: 'Input amount', value: `${numOfTokenSold} ${soldTokenName} ($0)`, inline: true },
-                    { name: 'Output amount', value: `${numOfTokenBought} ${boughtTokenName} ($0)`, inline: true },
+                    { name: 'Input amount', value: `${numOfTokenSold} ${soldTokenName} (${totalUSDValueSold})`, inline: true },
+                    { name: 'Output amount', value: `${numOfTokenBought} ${boughtTokenName} (${totalUSDValueBought})`, inline: true },
                 )
                 .addField(`Fees gained by LPs`, "0", false)
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            if (!isProduction) {
+                embed = embed.setFooter(`Hardhat network`)
+            }
 
             send(embed);
             log(JSON.stringify(event));
         });
 
         // On AddLiquidity event
-        instance.on("AddLiquidity", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
-            let depositAmounts = "";
-
+        instance.on("AddLiquidity", async (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
             const digitsToShow = 3
-            for (let i = 0; i < tokenAmounts.length; i++) {
-                depositAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]} , `
-            }
-            depositAmounts = depositAmounts.slice(0, -2)
 
-            const totalDollarValue = 0;
+            const depositAmounts = tokenAmounts.map((amount, i) =>
+                `${toHumanString(amount, contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]}`
+            ).join(', ')
+
+            const prices = await queryTokenPricesUSD(contract["coingeckoIDs"])
+            const totalDollarValue = tokenAmounts.map((amount, i) =>
+                toHumanString(BigNumber.from(amount).mul(prices[`${contract["coingeckoIDs"][i]}`]["usd"]), contract["decimals"][i], digitsToShow)
+            ).reduce((a, val) => a + parseFloat(val), 0)
 
             // inside a command, event listener, etc.
-            const embed = new MessageEmbed()
+            let embed = new MessageEmbed()
                 .setColor('#33ff33')
                 .setTitle('Deposit')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
@@ -102,28 +142,34 @@ async function main() {
                 .setDescription(`${provider} added new liquidity to the ${contract['name']}`)
                 .addFields(
                     { name: 'Deposit amounts', value: `${depositAmounts}`, inline: false },
-                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+                    {name: "Total USD value", value: `${toUSD(totalDollarValue)}`, inline: false}
                 )
                 .setTimestamp()
                 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            if (!isProduction) {
+                embed = embed.setFooter(`Hardhat network`)
+            }
 
             send(embed);
             log(JSON.stringify(event));
         });
 
         // On RemoveLiquidity event
-        instance.on("RemoveLiquidity", (provider, tokenAmounts, lpTokenSupply, event) => {
-            let withdrawAmounts = "";
-
+        instance.on("RemoveLiquidity", async (provider, tokenAmounts, lpTokenSupply, event) => {
             const digitsToShow = 3
-            for (let i = 0; i < tokenAmounts.length; i++) {
-                withdrawAmounts += `${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]}, `
-            }
-            withdrawAmounts = withdrawAmounts.slice(0, -2)
-            const totalDollarValue = 0;
+
+            const withdrawAmounts = tokenAmounts.map((amount, i) =>
+                `${toHumanString(amount, contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]}`
+            ).join(', ')
+
+            const prices = await queryTokenPricesUSD(contract["coingeckoIDs"])
+            const totalDollarValue = tokenAmounts.map((amount, i) =>
+                toHumanString(BigNumber.from(amount).mul(prices[`${contract["coingeckoIDs"][i]}`]["usd"]), contract["decimals"][i], digitsToShow)
+            ).reduce((a, val) => a + parseFloat(val), 0)
 
             // inside a command, event listener, etc.
-            const embed = new MessageEmbed()
+            let embed = new MessageEmbed()
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
@@ -131,25 +177,30 @@ async function main() {
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
                     { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
-                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+                    {name: "Total USD value", value: `${toUSD(totalDollarValue)}`, inline: false}
 
                 )
                 .setTimestamp()
-                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            if (!isProduction) {
+                embed = embed.setFooter(`Hardhat network`)
+            }
 
             send(embed);
             log(JSON.stringify(event));
         });
 
         // On RemoveLiquidity event
-        instance.on("RemoveLiquidityOne", (provider, lpTokenAmount, lpTokenSupply, boughtId, tokensBought, event) => {
+        instance.on("RemoveLiquidityOne", async (provider, lpTokenAmount, lpTokenSupply, boughtId, tokensBought, event) => {
             const digitsToShow = 3
             const withdrawAmounts = `${toHumanString(tokensBought, contract["decimals"][boughtId], digitsToShow)} ${contract["tokens"][boughtId]}`;
 
-            const totalDollarValue = 0
+            const prices = await queryTokenPricesUSD(contract["coingeckoIDs"])
+            const totalDollarValue =
+                toHumanString(BigNumber.from(tokensBought).mul(prices[`${contract["coingeckoIDs"][boughtId]}`]["usd"]), contract["decimals"][boughtId], digitsToShow)
 
             // inside a command, event listener, etc.
-            const embed = new MessageEmbed()
+            let embed = new MessageEmbed()
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
@@ -157,28 +208,33 @@ async function main() {
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
                     { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
-                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+                    {name: "Total USD value", value: `${toUSD(totalDollarValue)}`, inline: false}
 
                 )
                 .setTimestamp()
-                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            if (!isProduction) {
+                embed = embed.setFooter(`Hardhat network`)
+            }
 
             send(embed);
             log(JSON.stringify(event));
         });
 
         // On RemoveLiquidity event
-        instance.on("RemoveLiquidityImbalance", (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
-            let withdrawAmounts = "";
-
+        instance.on("RemoveLiquidityImbalance", async (provider, tokenAmounts, fees, invariant, lpTokenSupply, event) => {
             const digitsToShow = 4
-            for (let i = 0; i < tokenAmounts.length; i++) {
-                withdrawAmounts += `${contract["tokens"][i]} ${toHumanString(tokenAmounts[i], contract["decimals"][i], digitsToShow)}, `
-            }
-            withdrawAmounts = withdrawAmounts.slice(0, -2)
+            const withdrawAmounts = tokenAmounts.map((amount, i) =>
+                `${toHumanString(amount, contract["decimals"][i], digitsToShow)} ${contract["tokens"][i]}`
+            ).join(', ')
+
+            const prices = await queryTokenPricesUSD(contract["coingeckoIDs"])
+            const totalDollarValue = tokenAmounts.map((amount, i) =>
+                toHumanString(BigNumber.from(amount).mul(prices[`${contract["coingeckoIDs"][i]}`]["usd"]), contract["decimals"][i], digitsToShow)
+            ).reduce((a, val) => a + parseFloat(val), 0)
 
             // inside a command, event listener, etc.
-            const embed = new MessageEmbed()
+            let embed = new MessageEmbed()
                 .setColor('#FF9A00')
                 .setTitle('Withdraw')
                 .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
@@ -186,11 +242,14 @@ async function main() {
                 .setDescription(`${provider} removed liquidity from the ${contract['name']}`)
                 .addFields(
                     { name: 'Withdraw amounts', value: `${withdrawAmounts}`, inline: false },
-                    {name: "Total USD value", value: `${formatter.format(totalDollarValue)}`, inline: false}
+                    {name: "Total USD value", value: `${toUSD(totalDollarValue)}`, inline: false}
 
                 )
                 .setTimestamp()
-                .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+            if (!isProduction) {
+                embed = embed.setFooter(`Hardhat network`)
+            }
 
             send(embed);
             log(JSON.stringify(event));
